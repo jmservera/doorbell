6f026fbff3a410c211460724962722a8d9863977
jmservera <8036360+jmservera@users.noreply.github.com>
2022-01-12 10:28:11 +0100

    gpio controller

---
+++ controller/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo stopping service
+sudo systemctl stop doorbell-control.service
+
+echo install requirements
+sudo pip3 install -r requirements.txt
+
+echo install service
+sudo mkdir -p /opt/doorbell
+sudo cp controller.py /opt/doorbell
+sudo cp config.ini /opt/doorbell
+sudo \cp doorbell-control.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/doorbell-control.service
+sudo systemctl daemon-reload
+sudo systemctl enable doorbell-control.service
+sudo systemctl restart doorbell-control.service
+
+echo waiting for service start
+sleep 8
+
+journalctl -u doorbell-control -n 10
