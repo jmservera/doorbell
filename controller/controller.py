fa7c712f30b500524bebca1365aafe23fe0d6709
jmservera <8036360+jmservera@users.noreply.github.com>
2022-01-21 18:19:29 +0000

    extract functionalities to modules

--- controller/controller.py
+++ controller/controller.py
@@ -1,70 +1,16 @@
 #!/usr/bin/python3
 # Uses edge detection to limit the rate of Mqtt-messages
-
-import paho.mqtt.client as paho
-import time
-import RPi.GPIO as GPIO
 import sys
 
+import time
+import RPi.GPIO as GPIO
 
 from signal import pause
 
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-mqttc = None
-
-formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+from logger import logger
+from transport import sendmqtt, mqttConnect
 
 import configparser
-
-
-def sendmqtt(mess):
-    topic="doorbell/ding"
-    try:
-        logger.info("Message '"+mess+"' to "+topic)
-        mqttc.publish(topic, mess)
-    except:
-        logger.error("mqtt error")
-        logger.error(sys.exc_info()[1])
-        pass
-
-def on_disconnect(client, userdata, rc):
-    if rc != 0:
-        logger.warning("Unexpected MQTT disconnection. Will auto-reconnect")
-
-def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        logger.info("MQTT connected OK Returned code=" + str(rc))
-        mqttc.subscribe("doorbell/#",1)
-        logger.info("Subscribed to doorbell messages")
-    else:
-        logger.error("MQTT Bad connection Returned code=" + str(rc))
-
-def on_message(client, userdata, message):
-    logger.warning("Message arrived: " + message.topic + " '" + message.payload.decode() + "'")
-
-
-def mqttConnect(mqtt_user,mqtt_pass,mqtt_server,mqtt_port):
-    logger.info("MQTT trying connection to "+mqtt_server+":"+str(mqtt_port))
-
-    mqttc = paho.Client()
-    mqttc.username_pw_set(mqtt_user, mqtt_pass)
-
-    mqttc.connect(mqtt_server,mqtt_port)
-    mqttc.on_connect = on_connect
-    mqttc.on_message = on_message
-    mqttc.on_disconnect = on_disconnect
-    mqttc.loop_start()
-
-    logger.info("MQTT configured")
-
-    return mqttc
 
 ring_running = False
 ring_count = 0
@@ -107,7 +53,7 @@ def main(argv):
 
 
     # Mqtt
-    mqttc = mqttConnect(mqtt_user,mqtt_pass,mqtt_server,mqtt_port)
+    mqttConnect(mqtt_user,mqtt_pass,mqtt_server,mqtt_port)
 
 
     # Setup Gpio
@@ -116,7 +62,7 @@ def main(argv):
     GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)          # Output from Doorbell
     GPIO.setup(output_pin,GPIO.OUT)         # Open door relay
 
-    logger.info("GPI configured in pin: "+str(input_pin))
+    logger.info("GPI configured in pin: " + str(input_pin))
 
 
     logger.info("Starting service")
@@ -125,7 +71,7 @@ def main(argv):
     GPIO.add_event_detect(input_pin, GPIO.FALLING, callback=ring_callback)
     pause()
 
-    mqttc.loop_stop()
+    stop_transport()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ controller/logger.py
@@ -0,0 +1,15 @@
+import sys
+import logging
+
+logger=None
+
+if __name__ == "logger":
+    if logger == None:
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
+        handler = logging.StreamHandler(stream=sys.stdout)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.info("Logger configured")
